d2e8310fff9ddabee50dac7df708519a890de80a
sassofrancesco00 <47978951+sassofrancesco00@users.noreply.github.com>
2025-05-20 18:54:25 +0200

    First Commit.
    First Commit.

---
+++ backend/src/test/java/com/tesi/francescosasso/finapi/fin_api/FinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesi.francescosasso.finapi.fin_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
